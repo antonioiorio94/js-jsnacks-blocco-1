7d21062a2b46ce274f4f6a1ebf3c0596269c53d6
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-10 15:10:13 +0100

    init primo esercizio

---
+++ jsnack1/script.js
@@ -0,0 +1,10 @@
+//JSSnack
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+//creazione lista invitati
+const listaInvitati = [Angela,Beatrice,Camilla,Dario,Ernesto,Fernanda,Giuseppe];
+
+
+//controllo della lista tramite ciclo for
+for (let i=0; i<listaInvitati.length;i++)
