fc38a879c3f11c365290c0e95a49df2f3f1a6707
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-10 16:00:33 +0100

    secondo esercizio completo

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,11 +1,24 @@
 //JSSnack2
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
 
-
+let elencoNumeriDispari = [];
 
 //Crea un loop per contare fino a 6
 
+for(let i=0; i<6; i++){
 
-//Chiedi all'utente un numero
+  //Chiedi all'utente un numero
 
-//if con controllo resto + push nell'array
+  let numeroUtente= parseInt(prompt("Digita un numero"));
+  
+  //if con controllo resto 
+    if(numeroUtente % 2 === 1){
+      //push nell'array
+      elencoNumeriDispari.push(numeroUtente);
+    }
+   
+}
+
+console.log(elencoNumeriDispari);
+
+
