eea212664d47ee1d5c180df69ff3cbdec5c2f8dc
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-10 15:29:26 +0100

    primo esercizio completo

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,8 +3,27 @@
 
 
 //creazione lista invitati
-const listaInvitati = [Angela,Beatrice,Camilla,Dario,Ernesto,Fernanda,Giuseppe];
+const listaInvitati = ["Angela","Beatrice","Camilla","Dario","Ernesto","Fernanda","Giuseppe"];
 
+//richiesta del nome all'utente
+let nomeOspite = prompt("Buonasera, può dirmi il suo nome?").trim();
+
+//variabile esito controllo
+let esitoControllo = false;
 
 //controllo della lista tramite ciclo for
-for (let i=0; i<listaInvitati.length;i++)
+for (let i=0; i<listaInvitati.length; i++){
+  
+  if(nomeOspite === listaInvitati[i]) {
+    esitoControllo = true;
+  }
+}
+
+
+if (esitoControllo){
+ console.log("Prego, si goda la festa!");
+}else{
+  console.log("Mi dispiace, il suo nome non è sulla lista!")
+
+}
+
